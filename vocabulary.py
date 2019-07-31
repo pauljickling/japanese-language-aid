ff8c384a7ff0ce5714f7ad26dd5aaebeb63bb8aa
Paul Jickling <pjickling@gmail.com>
2019-07-31 15:48:05 -0700

    Verb list now guaranteed unique

--- vocabulary.py
+++ vocabulary.py
@@ -17,8 +17,10 @@ verbs_arr = [v for i, v in enumerate(verbs)]
 random_verbs = []
 n = 10
 while n > 0:
-    random_verbs.append(random.choice(verbs_arr))
-    n -= 1
+    random_verb = random.choice(verbs_arr)
+    if random_verb not in random_verbs:
+        random_verbs.append(random_verb)
+        n -= 1
 
 print("Define the following verbs:")
 for verb in random_verbs:
